6177618962b72e5a8de032ff4c1adf493ee1b6c5
Somkong <jeongu1270@gmail.com>
2019-08-21 17:01:52 +0900

    프로필 사진 추가 가능. 권한 획득 코드 추가.

--- app/src/main/java/com/example/sba_project/Register/Additional_data.java
+++ app/src/main/java/com/example/sba_project/Register/Additional_data.java
@@ -2,11 +2,22 @@ package com.example.sba_project.Register;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
-import android.service.autofill.UserData;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,15 +31,24 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.IOException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class Additional_data extends AppCompatActivity implements View.OnClickListener {
-    TextView NickName;
-    TextView Age;
-    TextView Address;
-    ValueEventListener listener;
-    DatabaseReference users_ref;
+    private TextView NickName;
+    private TextView Age;
+    private TextView Address;
+    private CircleImageView ProifleImg;
+    private ValueEventListener listener;
+    private DatabaseReference users_ref;
+
+    private String cameraPermission[];
+    private String storagePermission[];
 
     private static final int SEARCH_ADDRESS_ACTIVITY = 10000;
-
+    private static final int GET_FROM_GALLERY = 100;
+    private static final int STORAGE_REQUEST_CODE = 400;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,6 +73,15 @@ public class Additional_data extends AppCompatActivity implements View.OnClickLi
 //                Intent i = new Intent(Additional_data.this, WebviewSearchaddress.class);
 //                startActivityForResult(i, SEARCH_ADDRESS_ACTIVITY);
                 break;
+            case R.id.registerphoto:
+                if (!checkStoragePermission()) {
+                    //Storage permission not allowed, request it
+                    requestStoragePermission();
+                }
+                else{
+                    GetImageFromGallery();
+                }
+                break;
         }
     }
 
@@ -60,10 +89,18 @@ public class Additional_data extends AppCompatActivity implements View.OnClickLi
         findViewById(R.id.btn_additionalRegister).setOnClickListener(this);
         findViewById(R.id.btn_nexttime).setOnClickListener(this);
         findViewById(R.id.btn_searchaddress).setOnClickListener(this);
+        findViewById(R.id.registerphoto).setOnClickListener(this);
 
+        ProifleImg = (CircleImageView) findViewById(R.id.profilephoto);
         NickName = findViewById(R.id.NickName);
         Age = findViewById(R.id.Age);
         Address = findViewById(R.id.Address);
+
+        //camera permission
+        cameraPermission = new String[]{Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+        storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
     }
 
     void AdditionalRegister() {
@@ -90,29 +127,27 @@ public class Additional_data extends AppCompatActivity implements View.OnClickLi
     }
 
     // nickname 검사
-    private boolean CheckNickName(final String newNickName){
-        if(users_ref == null)
-        {
+    private boolean CheckNickName(final String newNickName) {
+        if (users_ref == null) {
             users_ref = FirebaseDatabase.getInstance().getReference().child("users");
         }
 
-        class RS{
+        class RS {
             boolean result = true;
         }
 
         final RS rs = new RS();
 
         // 비동기일 경우 다르게 처리해야함.
-        listener =  users_ref.addValueEventListener(new ValueEventListener() {
+        listener = users_ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     // mMyAdapter.addItem(ContextCompat.getDrawable(getApplicationContext(), R.drawable.icon), "name_" + i, "contents_" + i);
                     MyUserData tmp = snapshot.getValue(MyUserData.class);
 
                     // 동일한 닉네임이 이미 존재한다면..
-                    if(!tmp.NickName.isEmpty() && tmp.NickName.equals(newNickName))
-                    {
+                    if (!tmp.NickName.isEmpty() && tmp.NickName.equals(newNickName)) {
                         Toast.makeText(getApplicationContext(), tmp.NickName + " already exists.", Toast.LENGTH_SHORT).show();
                         rs.result = false;
                     }
@@ -145,6 +180,73 @@ public class Additional_data extends AppCompatActivity implements View.OnClickLi
 
                 }
                 break;
+            case GET_FROM_GALLERY: {
+                sendPicture(intent.getData()); //갤러리에서 가져오기
+            }
+            break;
         }
+    }
+
+    private void GetImageFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setData(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        startActivityForResult(intent, GET_FROM_GALLERY);
+    }
+
+    private void sendPicture(Uri imgUri) {
+        String imagePath = getRealPathFromURI(imgUri); // path 경로
+        ExifInterface exif = null;
+        try {
+            exif = new ExifInterface(imagePath);
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+        int exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+        int exifDegree = exifOrientationToDegrees(exifOrientation);
+        Bitmap bitmap = BitmapFactory.decodeFile(imagePath); //경로를 통해 비트맵으로 전환
+        ProifleImg.setImageBitmap(rotate(bitmap, exifDegree));
+    }
+
+    private int exifOrientationToDegrees(int exifOrientation) {
+        if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
+            return 90;
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
+            return 180;
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
+            return 270;
+        }
+        return 0;
+    }
+
+    private Bitmap rotate(Bitmap src, float degree) {
+        // Matrix 객체 생성
+        Matrix matrix = new Matrix();
+        // 회전 각도 셋팅
+        matrix.postRotate(degree);
+        // 이미지와 Matrix 를 셋팅해서 Bitmap 객체 생성
+        return Bitmap.createBitmap(src, 0, 0, src.getWidth(),
+                src.getHeight(), matrix, true);
+    }
+
+    private String getRealPathFromURI(Uri contentUri) {
+        int column_index = 0;
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
+        if (cursor.moveToFirst()) {
+            column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        }
+        return cursor.getString(column_index);
+    }
+
+    private void requestStoragePermission() {
+        ActivityCompat.requestPermissions(this, storagePermission, STORAGE_REQUEST_CODE);
+    }
+
+    private boolean checkStoragePermission() {
+        boolean result = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        return result;
     }
 }
