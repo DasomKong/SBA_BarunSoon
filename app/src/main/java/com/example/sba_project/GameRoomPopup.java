0f07b5b0b5450d42c68dde69389ed8729678d5d9
ADMINSTRATOR-PC\adminstrator <rbtjd5087@naver.com>
2019-08-22 09:45:34 +0900

    daily excute

---
+++ app/src/main/java/com/example/sba_project/GameRoomPopup.java
@@ -0,0 +1,41 @@
+package com.example.sba_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+public class GameRoomPopup extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mConfirm;
+    private Button mCancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_game_room_popup);
+
+        setContent();
+    }
+    private void setContent() {
+        mConfirm = (Button) findViewById(R.id.btnConfirm);
+        mCancel = (Button) findViewById(R.id.btnCancel);
+
+        mConfirm.setOnClickListener(this);
+        mCancel.setOnClickListener(this);
+    }
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnConfirm:
+                this.finish();
+                break;
+            case R.id.btnCancel:
+                this.finish();
+                break;
+            default:
+                break;
+        }
+    }
+}
