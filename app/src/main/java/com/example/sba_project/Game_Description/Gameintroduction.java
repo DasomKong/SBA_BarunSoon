d9cba79a719c1290d355d70af6d369e625ff755e
Sooooooooon <49817827+Sooooooooon@users.noreply.github.com>
2019-08-25 15:13:12 +0900

    이미지 동영상 분류 gameintroduction파일

--- app/src/main/java/com/example/sba_project/Game_Description/Gameintroduction.java
+++ app/src/main/java/com/example/sba_project/Game_Description/Gameintroduction.java
@@ -2,6 +2,7 @@ package com.example.sba_project.Game_Description;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -21,29 +22,84 @@ import com.google.firebase.database.ValueEventListener;
 //YouTubeBaseActivity로 상속 받는것에 유의
 public class Gameintroduction extends YouTubeBaseActivity {
 
-
-
-    YouTubePlayerView youTubeView;
-    YouTubePlayer.OnInitializedListener listener;
-
     FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     DatabaseReference databaseReference = firebaseDatabase.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sub);
 
 
         Intent intent = getIntent();
-        String title = intent.getExtras().getString("title");
-        System.out.println("abcd" + title);
-
+        final String title = intent.getExtras().getString("title");
 
         databaseReference.child("Game").child("title").child(title).addValueEventListener(new ValueEventListener() {
 
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                final String touchdownVideo = (String) dataSnapshot.child("URL").getValue();
+
+
+                if(touchdownVideo.equals("NULL")){
+                    setContentView(R.layout.activity_gameintroduction_image);
+                    ImageView gameimage = (ImageView)findViewById(R.id.gameimage);
+
+                    switch (title) {
+                        case "TWINS":
+                            gameimage.setImageResource(R.drawable.twins1);
+                            break;
+                        case "DOJO":
+                            gameimage.setImageResource(R.drawable.dojo);
+                            break;
+                        case "PILA":
+                            gameimage.setImageResource(R.drawable.pila1);
+                            break;
+                        case "SCOREBOARD":
+                            gameimage.setImageResource(R.drawable.scboard1);
+                            break;
+                        case "CHRONO":
+                            gameimage.setImageResource(R.drawable.chrono1);
+                            break;
+                        case "RELE":
+                            gameimage.setImageResource(R.drawable.rele1);
+                            break;
+                        case "WAK":
+                            gameimage.setImageResource(R.drawable.wak1);
+                            break;
+                    }
+
+
+                }
+                else {
+                    setContentView(R.layout.activity_sub);
+
+                    YouTubePlayerView youTubeView;
+                    YouTubePlayer.OnInitializedListener listener;
+                    youTubeView = (YouTubePlayerView) findViewById(R.id.youtubeView);
+
+
+                    //리스너 등록부분
+                    listener = new YouTubePlayer.OnInitializedListener() {
+
+                        //초기화 성공시
+                        @Override
+                        public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                            youTubePlayer.loadVideo(touchdownVideo);//url의 맨 뒷부분 ID값만 넣으면 됨
+
+                        }
+
+                        @Override
+                        public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+                        }
+
+                    };
+
+                    youTubeView.initialize("AIzaSyDOmjYJs_Uly-x-lG9NNUvNSiwp8Fr3M_8", listener);
+
+
+                }
 
                 String touchdownName = (String) dataSnapshot.getKey();
                 TextView nameView = (TextView)findViewById(R.id.nameView);
@@ -64,39 +120,6 @@ public class Gameintroduction extends YouTubeBaseActivity {
                 String touchdownTarget = (String) dataSnapshot.child("Target_Age").getValue();
                 TextView targetView = (TextView)findViewById(R.id.targetView);
                 targetView.setText(touchdownTarget);
-
-                final String touchdownVideo = (String) dataSnapshot.child("URL").getValue();
-
-
-
-
-//                mWebView.loadUrl(gaiaVideo);
-
-                //System.out.println("aaa"+dataSnapshot.getValue());
-
-                youTubeView = (YouTubePlayerView)findViewById(R.id.youtubeView);
-
-
-                //리스너 등록부분
-                listener = new YouTubePlayer.OnInitializedListener(){
-
-                    //초기화 성공시
-                    @Override
-                    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-                        youTubePlayer.loadVideo(touchdownVideo);//url의 맨 뒷부분 ID값만 넣으면 됨
-
-                    }
-
-                    @Override
-                    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
-
-                    }
-
-                };
-
-                youTubeView.initialize("AIzaSyDOmjYJs_Uly-x-lG9NNUvNSiwp8Fr3M_8", listener);
-
-
 
             }
 
